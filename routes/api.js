da8e327fba095983a7ca90bc43d28063db307fd1
RGibesh <ralphgibendi01@gmail.com>
2022-03-10 19:21:35 +0300

    edited api file

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const router = express.Router();
+const Todo = require('../models/todo');
 
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+router.get('/todos', (req, res, next) => {
+  // This will return all the data, exposing only the id and action field to the client
+  Todo.find({}, 'action')
+    .then((data) => res.json(data))
+    .catch(next);
 });
 
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
+router.post('/todos', (req, res, next) => {
+  if (req.body.action) {
+    Todo.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: 'The input field is empty',
+    });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+router.delete('/todos/:id', (req, res, next) => {
+  Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
 });
+
+module.exports = router;
+
